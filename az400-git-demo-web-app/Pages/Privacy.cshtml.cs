1fad3f632cbcb2a2965d0693074c23814316c2c2
mpflynnx <mpflynnx@gmail.com>
2023-05-22 08:34:37 +0100

    Add project files.

---
+++ az400-git-demo-web-app/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace az400_git_demo_web_app.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
